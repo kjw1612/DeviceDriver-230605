2a3e0779a832c245db59307a99e419b3e8f0ed9b
jiwnoo <jori3597@naver.com>
2023-06-05 15:36:50 +0900

    read 구현 완료

--- Project10/DeviceDriver.cpp
+++ Project10/DeviceDriver.cpp
@@ -1,12 +1,24 @@
 #include "DeviceDriver.h"
+#include <Windows.h>
+#include <exception>
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int firstReadTryValue = m_hardware->read(address);
+
+    for (int i = 1;i < TotalReadTryCount; i++)
+    {
+        Sleep(200);
+        int nextReadTryValue = m_hardware->read(address);
+        if (firstReadTryValue != nextReadTryValue)
+        {
+            throw std::exception("ReadFailException!!");
+        }        
+    }
+    return firstReadTryValue;
 }
 
 void DeviceDriver::write(long address, int data)
--- Project10/DeviceDriver.h
+++ Project10/DeviceDriver.h
@@ -10,4 +10,5 @@ public:
 
 protected:
     FlashMemoryDevice* m_hardware;
+    const int TotalReadTryCount = 5;
 };
